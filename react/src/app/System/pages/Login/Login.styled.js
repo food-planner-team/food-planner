eac6e798e10a1e3adfbeba8b34d3528e1837cf25
enko <pytlos1337@gmail.com>
2023-02-01 14:52:23 +0100

    Add theme and UnauthorizedLayaout

--- react/src/app/System/pages/Login/Login.styled.js
+++ react/src/app/System/pages/Login/Login.styled.js
@@ -1,67 +1,7 @@
 import styled, { keyframes } from "styled-components";
 
-export const animationBgElement = keyframes`
-    from {
-        transform: rotate(120deg);
-
-    }
-
-    to {
-
-        transform: rotate(160deg);
-    }
-`;
-
-export const FormContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    min-height: 100vh;
-    position: relative;
-    overflow: hidden;
-
-    &:after {
-        content: "";
-        position: absolute;
-        width: 550px;
-        height: 550px;
-        left: -14rem;
-        bottom: -14rem;
-        border-radius: 154px 123px 234px 520px;
-        transform: rotate(100deg);
-        background: linear-gradient(
-            to top,
-            rgba(70, 110, 102, 0.8),
-            transparent
-        );
-        z-index: -1;
-        box-shadow: 0px 0px 45px rgba(70, 110, 102, 0.1);
-        animation: ${animationBgElement} 8s ease infinite alternate;
-    }
-
-    &:before {
-        content: "";
-        position: absolute;
-        width: 450px;
-        height: 450px;
-        right: -14rem;
-        top: -14rem;
-        border-radius: 54px 123px 434px 20px;
-        transform: rotate(180deg);
-        background: linear-gradient(
-            to top,
-            rgba(70, 110, 102, 0.4),
-            transparent
-        );
-        z-index: -1;
-        box-shadow: 0px 0px 45px rgba(70, 110, 102, 0.1);
-        animation: ${animationBgElement} 8s ease infinite alternate;
-    }
-`;
-
 export const Panel = styled.div`
-    box-shadow: 0px 16px 30px #7090b020;
+    box-shadow: 0px 16px 30px ${({ theme }) => theme.colors.boxShadow};
     border-radius: 16px;
     width: 800px;
     min-height: 500px;
@@ -69,7 +9,7 @@ export const Panel = styled.div`
     padding: 2rem;
     box-sizing: border-box;
     backdrop-filter: blur(20px);
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: ${({ theme }) => theme.colors.backgroundAlpha};
 `;
 
 export const PanelWrapper = styled.div`
@@ -97,7 +37,7 @@ export const HeaderTitle = styled.div`
         position: absolute;
         width: 40%;
         height: 1px;
-        background-color: #ddd;
+        background-color: ${({ theme }) => theme.colors.white};
         top: 50%;
         margin-left: 1.5rem;
     }
@@ -132,11 +72,11 @@ export const GroupTitle = styled.div`
 
 export const GroupError = styled.div`
     flex: 2;
-    color: rgb(224, 37, 37);
+    color: ${({ theme }) => theme.colors.error};
     text-transform: uppercase;
     font-weight: 700;
     font-size: 10px;
-    background-color: rgb(255, 236, 236);
+    background-color: ${({ theme }) => theme.colors.errorBackground};
     padding: 0.6rem 1rem;
     border-radius: 12px;
     margin-top: 10px;
@@ -155,7 +95,7 @@ export const FormInput = styled.input`
     padding: 0.7rem 1rem;
     border: none;
     border-radius: 24px;
-    border: 1px solid #d2d2d2;
+    border: 1px solid ${({ theme }) => theme.colors.white};
 `;
 
 export const FormGroup = styled.div`
@@ -183,13 +123,13 @@ export const ButtonPrimary = styled.button`
     font-size: 11px;
     letter-spacing: 0.3px;
     transition: 0.45s ease;
-    background-color: rgb(70, 110, 102);
-    border: 1px solid rgb(70, 110, 102);
+    background-color: ${({ theme }) => theme.colors.primary};
+    border: 1px solid ${({ theme }) => theme.colors.primary};
     color: white;
 
     &:hover {
-        background-color: rgb(60, 82, 77);
-        border: 1px solid rgb(60, 82, 77);
+        background-color: ${({ theme }) => theme.colors.primaryAccent};
+        border: 1px solid ${({ theme }) => theme.colors.primaryAccent};
     }
 `;
 
@@ -205,12 +145,12 @@ export const ButtonSecondary = styled.input`
     letter-spacing: 0.3px;
     transition: 0.45s ease;
     background-color: transparent;
-    border: 1px solid rgb(70, 110, 102);
-    color: rgb(70, 110, 102);
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.primary};
 
     &:hover {
-        background-color: rgb(70, 110, 102);
-        border: 1px solid rgb(70, 110, 102);
+        background-color: ${({ theme }) => theme.colors.primary};
+        border: 1px solid ${({ theme }) => theme.colors.primary};
         color: white;
     }
 `;
@@ -235,6 +175,6 @@ export const FooterElement = styled.div`
 
 export const ElementHighlight = styled.span`
     font-weight: 700;
-    color: #333;
+    color: ${({ theme }) => theme.colors.typographyDark};
     text-transform: uppercase;
 `;
