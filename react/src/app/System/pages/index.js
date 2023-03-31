5234b372a023f4e6f05f75995cf3ec97e4414a49
Bartłomiej Pytlos <97595757+enkooo@users.noreply.github.com>
2023-03-31 20:40:38 +0200

    Delete react

--- react/src/app/System/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { NotFound } from "./NotFound.jsx";
-export { Home } from "./Home.jsx";
-export { Register } from "./Register/Register";
-export { Login } from "./Login/Login";
--- react/src/plugins/router/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import { SystemRoutes } from "../../app/System/routes";
-
-export const AllRoutes = () => (
-    <React.Fragment>
-        <SystemRoutes />
-    </React.Fragment>
-);
