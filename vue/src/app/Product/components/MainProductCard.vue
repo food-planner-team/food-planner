5e49a647560ab503efeeddef8ff4f8bd1a5d5c52
enko <pytlos1337@gmail.com>
2023-04-04 13:58:32 +0200

    Add mobile view on products page

--- vue/src/app/Product/components/MainProductCard.vue
+++ vue/src/app/Product/components/MainProductCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="h-56 w-[23rem] rounded-lg flex items-center gap-5 p-5 shadow-md bg-[#fefffe]"
+        class="h-56 w-[22rem] rounded-lg flex items-center gap-5 p-5 shadow-md bg-[#fefffe]"
     >
         <div
             class="w-[120px] h-[120px] rounded-md ml-1 flex items-center"
