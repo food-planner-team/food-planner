41b132e123391d1b9a2bd925cf83bd56529f928f
enko <pytlos1337@gmail.com>
2023-04-15 22:46:25 +0200

    Fix products container height

--- vue/src/app/Product/pages/AddProductPage.vue
+++ vue/src/app/Product/pages/AddProductPage.vue
@@ -1,68 +1,72 @@
 <template>
     <main class="main">
         <div class="wrapper">
-            <h1 class="font-bold text-2xl m-5 pl-3">
-                Dodawanie nowego produktu
-            </h1>
-            <div class="m-5 pl-3">
-                <p class="pb-2 text-sm text-gray-500">
-                    <label for="search">NAZWA PRODUKTU</label>
-                </p>
-                <div class="flex flex-wrap gap-6">
-                    <input
-                        type="search"
-                        id="search"
-                        class="rounded-lg w-full md:w-[29rem]"
-                        placeholder="Wyszukaj"
-                        v-model="searchValue"
-                        @keyup.enter="searchValue.length && getProducts()"
-                    />
-                    <div
-                        class="flex gap-6 flex-wrap justify-center w-full md:w-auto"
-                    >
-                        <button
-                            class="hidden lg:inline-flex justify-center rounded-md border border-transparent bg-primary-dark px-12 w-[200px] py-2 text-base font-medium text-white hover:bg-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 items-center"
-                            @click="searchValue.length && getProducts()"
-                        >
-                            Szukaj
-                        </button>
-                        <AddMainProductModal
-                            :chosen-products="chosenProducts"
-                            v-model:product-name="searchValue"
-                            v-model:products="products"
+            <div class="h-full flex flex-col">
+                <h1 class="font-bold text-2xl m-5 pl-3">
+                    Dodawanie nowego produktu
+                </h1>
+                <div class="m-5 pl-3">
+                    <p class="pb-2 text-sm text-gray-500">
+                        <label for="search">NAZWA PRODUKTU</label>
+                    </p>
+                    <div class="flex flex-wrap gap-6">
+                        <input
+                            type="search"
+                            id="search"
+                            class="rounded-lg w-full md:w-[29rem]"
+                            placeholder="Wyszukaj"
+                            v-model="searchValue"
+                            @keyup.enter="searchValue.length && getProducts()"
                         />
+                        <div
+                            class="flex gap-6 flex-wrap justify-center w-full md:w-auto"
+                        >
+                            <button
+                                class="hidden lg:inline-flex justify-center rounded-md border border-transparent bg-primary-dark px-12 w-[200px] py-2 text-base font-medium text-white hover:bg-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 items-center"
+                                @click="searchValue.length && getProducts()"
+                            >
+                                Szukaj
+                            </button>
+                            <AddMainProductModal
+                                :chosen-products="chosenProducts"
+                                v-model:product-name="searchValue"
+                                v-model:products="products"
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div
-                class="h-auto min-h-[35vh] lg:h-[550px] 3xl:h-[57rem] lg:overflow-y-scroll relative justify-items-center pb-5 pt-5"
-                :class="
-                    products.length > 3
-                        ? 'grid products-grid'
-                        : 'flex flex-wrap gap-10 justify-center lg:justify-start ml-8 mr-8'
-                "
-                ref="scrollComponent"
-            >
-                <template v-if="!isLoading">
-                    <ProductCard
-                        v-for="product in products"
-                        :key="product.id"
-                        :product="product"
-                        :chosen-products="chosenProducts"
-                        @choose-product="handleChooseProduct"
-                        @remove-product="handleRemoveProduct"
-                    />
-                </template>
-                <Loader
-                    v-else
-                    class="m-auto col-start-2 col-span-2 3xl:col-start-3 3xl:col-span-1"
-                />
-                <p
-                    v-if="!searchValue && !products.length"
-                    class="m-auto col-start-2 col-span-2 3xl:col-start-3 3xl:col-span-1"
-                >
-                    Wpisz nazwę i wyszukaj produkt!
-                </p>
+                <div class="h-full relative">
+                    <div
+                        class="lg:overflow-y-scroll relative justify-items-center pb-5 pt-1 lg:absolute left-0 top-0 h-full w-full"
+                        :class="
+                            products.length > 3
+                                ? 'grid products-grid'
+                                : 'flex flex-wrap gap-10 justify-center lg:justify-start ml-8 mr-8'
+                        "
+                        ref="scrollComponent"
+                    >
+                        <template v-if="!isLoading">
+                            <ProductCard
+                                v-for="product in products"
+                                :key="product.id"
+                                :product="product"
+                                :chosen-products="chosenProducts"
+                                @choose-product="handleChooseProduct"
+                                @remove-product="handleRemoveProduct"
+                            />
+                        </template>
+                        <Loader
+                            v-else
+                            class="m-auto col-start-2 col-span-2 3xl:col-start-3 3xl:col-span-1"
+                        />
+                        <p
+                            v-if="!searchValue && !products.length"
+                            class="m-auto col-start-2 col-span-2 3xl:col-start-3 3xl:col-span-1"
+                        >
+                            Wpisz nazwę i wyszukaj produkt!
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
